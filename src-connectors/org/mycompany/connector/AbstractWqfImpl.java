0174391ce9ab9b0f712dcd4182f72b200b316563
pierrick.voulet <pierrick.voulet@bonitasoft.com>
2018-10-04 15:24:03 -0400

    Initial commit of '7.8-alpha-testing'

---
+++ src-connectors/org/mycompany/connector/AbstractWqfImpl.java
@@ -0,0 +1,13 @@
+package org.mycompany.connector;
+
+import org.bonitasoft.engine.connector.AbstractConnector;
+import org.bonitasoft.engine.connector.ConnectorValidationException;
+
+public abstract class AbstractWqfImpl extends AbstractConnector {
+
+	@Override
+	public void validateInputParameters() throws ConnectorValidationException {
+
+	}
+
+}
---
+++ src-filters/org/mycompany/connector/AbstractEwfwefeImpl.java
@@ -0,0 +1,13 @@
+package org.mycompany.connector;
+
+import org.bonitasoft.engine.filter.AbstractUserFilter;
+import org.bonitasoft.engine.connector.ConnectorValidationException;
+
+public abstract class AbstractEwfwefeImpl extends AbstractUserFilter {
+
+	@Override
+	public void validateInputParameters() throws ConnectorValidationException {
+
+	}
+
+}
